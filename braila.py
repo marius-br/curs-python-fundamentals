371f1948c6b2590cae2060e87ecdd9f30543045c
Marius Braila <marius.braila@codeoftalent.com>
2025-07-30 10:34:54 +0300

    am facut o modificare

--- braila.py
+++ braila.py
@@ -1,4 +1,10 @@
 print("Salut, eu sunt Marius!")
 print("Acesta este brach-ul meu")
 
+# am terminat munca
+# git pull - ca sa vad daca sunt modificari de la colegi
+# git pull - aduce modificari publate de colegi in localul meu
+# git commit - pregatesc munca mea pt a fi trimisa la srv. + adaug um un mesaj
+# git push - publica modificarile mele pe server.
+
 print("test row 4")

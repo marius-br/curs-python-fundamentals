c11d90cafe0f4f8d184c7b07c92c70b11466ebfe
ionutc84 <ionut.croitoru84@gmail.com>
2025-07-30 10:31:02 +0300

    am facut 2 modificari

--- croitoru.py
+++ croitoru.py
@@ -1,1 +1,7 @@
 print("Salut, eu sunt Ionut!")
+
+# am terminat munca
+# git pull - ca sa vad daca sunt modificari de la colegi
+# git pull - aduce modificari publicate de colegi in localul meu
+# # git commit - pregatesc munca mea pt a fi trimisa la server + adaug un mesaj
+# git push - publica modificarile mele pe server

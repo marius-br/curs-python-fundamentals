c6041c2ca510796228d30618176b85d7d161b762
norbertzsolt.agosto <agoston_norbert@hotmail.com>
2025-07-30 10:31:29 +0300

    Am publicat un fisier cu numele meu

--- agoston.py
+++ agoston.py
@@ -1,1 +1,8 @@
 print("Salut, eu sunt Norbert!")
+
+
+# am terminat munca
+# git pull - ca sa vad daca sunt modificari de la colegi
+# git pull - aduce modificari publicate de colegi in localul meu
+# git commit - pregatesc munca mea pentru a fi trimisa la server + adaug un mesaj
+# git push - publica modificarile mele pe server

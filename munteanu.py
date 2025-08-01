933b5ebac693574c763e36b1a36fba3299d8067f
cristian.munteanu1 <cristian.munteanu1@gameloft.com>
2025-08-01 10:54:50 +0300

    not much but honest work

--- munteanu.py
+++ munteanu.py
@@ -6,7 +6,6 @@ while x != 2 or y != 3 or z !=4 or zz !=100:
     x = int(input("x: "))  # Ceri din nou valoarea pentru x
     y = int(input("y: "))  # Ceri din nou valoarea pentru y
     z = int(input("z: "))  # Ceri din nou valoarea pentru y
-    zz = int(input("zzz: "))  # Ceri din nou valoarea pentru y
+    zz = int(input("zz: "))  # Ceri din nou valoarea pentru y
 print("Condițiile au fost îndeplinite! x este 2, y este 3 z este 4 și zz este 100.")
 
-print("bla")
